2b99e53f896241c10871aaac1d22763c0b3696cd
Paulo Mattos <paulo.mattos@poli.ufrj.br>
2020-10-23 10:57:24 -0300

    Implemented solution for "The dining savages problem" (section 5.1)

--- section_5/source/main.c
+++ section_5/source/main.c
@@ -1,7 +1,60 @@
 #include "main.h"
+#include <pthread.h>
+
+#define CAPACITY 2
+
+volatile int servings = CAPACITY;
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t emptyCond = PTHREAD_COND_INITIALIZER;
+pthread_cond_t fullCond = PTHREAD_COND_INITIALIZER;
+
 
 int main()
-{
-    printf("Hello world!!\n");
+{                    
+    pthread_t cookThread;
+    pthread_create(&cookThread, NULL, cooker, NULL);
+    
+    pthread_t savageThreads[10];
+    for (int i = 0; i < 10; i++)
+    {
+        pthread_create(&savageThreads[i], NULL, savage, NULL);
+    }
+
+    // Join the cook thread so that the execution won't finish until the thread does
+    pthread_join(cookThread, NULL);
     return 0;
+}
+
+void *cooker()
+{
+    while(1)
+    {
+        pthread_mutex_lock(&mutex);
+        while (servings != 0)
+        {
+            pthread_cond_wait(&emptyCond, &mutex);
+        }
+        
+        servings = CAPACITY;
+        printf("Cozinheiro encheu o pote\n");
+        pthread_cond_signal(&fullCond);
+        pthread_mutex_unlock(&mutex);
+    }
+}
+
+void *savage()
+{
+    while(1)
+    {
+        pthread_mutex_lock(&mutex);
+        while (servings == 0)
+        {
+            pthread_cond_signal(&emptyCond);
+            pthread_cond_wait(&fullCond, &mutex);
+        }
+
+        servings--;
+        printf("Selvagem pegou uma porção\n");
+        pthread_mutex_unlock(&mutex);
+    }
 }
