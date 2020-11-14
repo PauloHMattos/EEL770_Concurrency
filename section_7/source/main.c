4a446b2acb19763eb0c20611358e97f2f3fe2e60
Paulo Mattos <paulo.mattos@poli.ufrj.br>
2020-11-14 18:25:38 -0300

    Implements The Sushi Bar problem (7.1)

--- section_7/source/main.c
+++ section_7/source/main.c
@@ -1,7 +1,75 @@
 #include "main.h"
 
+int eating = 0;
+pthread_mutex_t mtx = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t mutexFull = PTHREAD_MUTEX_INITIALIZER;
+
+void enter_bar(void *threadNumPtr)
+{
+    int threadNum = *((int *)threadNumPtr);
+    pthread_mutex_lock(&mutexFull);
+    pthread_mutex_lock(&mtx);
+    printf("<<<<<<<<<<: Cliente %i sentou\n", threadNum);
+    eating++;
+
+    if (eating < 5)
+    {
+        pthread_mutex_unlock(&mutexFull);
+    }
+    else
+    {
+        printf("##########: Todas as mesas estão cheias \n");
+    }
+    pthread_mutex_unlock(&mtx);
+
+    int threadId = pthread_self();
+
+    // Cria um tempo de atendimento "aleatório" usando o id da thread como seed
+    int eatingTime = getRandom(threadId, 100);
+
+    // Simula o tempo que o cliente leva para comer
+    waitFor(eatingTime);
+
+    // Sai do bar
+    exit_bar(threadNum);
+    free(threadNumPtr);
+}
+
+void exit_bar(int threadNum)
+{
+    pthread_mutex_lock(&mtx);
+    printf(">>>>>>>>>>: Cliente %i saiu\n", threadNum);
+    eating--;
+    if (eating == 0)
+    {
+        printf("##########: Mesa liberada \n");
+        pthread_mutex_unlock(&mtx);
+        pthread_mutex_unlock(&mutexFull);
+    }
+    else
+    {
+        pthread_mutex_unlock(&mtx);
+    }
+}
+
+#define CLIENTS 18
+
 int main()
 {
-    printf("Hello world!!\n");
+    pthread_t threads[CLIENTS];
+    for (int i = 0; i < CLIENTS; i++)
+    {
+        int *threadId = malloc(sizeof(*threadId));
+        *threadId = i;
+        pthread_create(&threads[i], NULL, enter_bar, (void *)threadId);
+    }
+
+    for (int i = 0; i < CLIENTS; i++)
+    {
+        pthread_t th = threads[i];
+        pthread_join(th, NULL);
+    }
+
+    printf("Fim\n");
     return 0;
 }
--- section_7/source/main.h
+++ section_7/source/main.h
@@ -1,2 +1,19 @@
+#include <pthread.h>
 #include <stdio.h>
+#include <unistd.h>
 
+#define MIN(a, b) (((a) < (b)) ? (a) : (b))
+
+int getRandom(int seed, int max)
+{
+    srand(time(NULL) + seed);
+    return rand() % max;
+}
+
+void waitFor(int tempo)
+{
+    for (int i = 0; i < tempo; i++)
+    {
+        usleep(100000);
+    }
+}
