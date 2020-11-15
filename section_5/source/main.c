b6b51d6c2250fdedf9ef363d2603fc18824123e2
Paulo Mattos <paulo.mattos@poli.ufrj.br>
2020-11-15 19:45:00 -0300

    Fix savages implementation to better suit the book example

--- section_5/source/main.c
+++ section_5/source/main.c
@@ -13,7 +13,7 @@ pthread_cond_t fullCond = PTHREAD_COND_INITIALIZER;
 int main()
 {                    
     pthread_t cookThread;
-    pthread_create(&cookThread, NULL, cooker, NULL);
+    pthread_create(&cookThread, NULL, cookerThread, NULL);
     
     pthread_t savageThreads[SAVAGES];
     int savageIds[SAVAGES];
@@ -21,7 +21,7 @@ int main()
     for (int i = 0; i < SAVAGES; i++)
     {
         savageIds[i] = i + 1;
-        pthread_create(&savageThreads[i], NULL, &savage, &savageIds[i]);
+        pthread_create(&savageThreads[i], NULL, savageThread, &savageIds[i]);
     }
 
     // Join the cook thread so that the execution won't finish until the thread does
@@ -29,7 +29,7 @@ int main()
     return 0;
 }
 
-void cooker()
+void *cookerThread()
 {
     while(1)
     {
@@ -46,21 +46,33 @@ void cooker()
     }
 }
 
-void savage(void* savageIdPtr)
+void *savageThread(void* savageIdPtr)
 {
     int savageId = *(int*)savageIdPtr;
     while(1)
     {
-        pthread_mutex_lock(&mutex);
-        while (servings == 0)
-        {
-            pthread_cond_signal(&emptyCond);
-            pthread_cond_wait(&fullCond, &mutex);
-        }
-
-        servings--;
-        printf("[Selvagem] %i pegou uma porção\n", savageId);
-        pthread_mutex_unlock(&mutex);
-        usleep(100000);
+        getServingFromPot(savageId);
+        eat(savageId);
     }
+}
+
+void getServingFromPot(int savageId)
+{
+    pthread_mutex_lock(&mutex);
+    while (servings == 0)
+    {
+        pthread_cond_signal(&emptyCond);
+        pthread_cond_wait(&fullCond, &mutex);
+    }
+
+    servings--;
+    printf("[Selvagem] %i pegou uma porção\n", savageId);
+    pthread_mutex_unlock(&mutex);
+}
+
+void eat(int savageId)
+{
+    // Uses the savage id as a seed for the random number generator
+    int sleepTime = getRandom(savageId, 10);
+    waitFor(sleepTime);
 }
--- section_5/source/main.h
+++ section_5/source/main.h
@@ -1,5 +1,24 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <time.h>
+#include <stdlib.h>
 
+void *cookerThread();
+void *savageThread(void* savageIdPtr);
 
-void cooker();
-void savage(void* savageIdPtr);
+void eat(int savageId);
+void getServingFromPot(int savageId);
+
+int getRandom(int seed, int max)
+{
+    srand(time(NULL) + seed);
+    return rand() % max;
+}
+
+void waitFor(int timeInSeconds)
+{
+    for (int i = 0; i < timeInSeconds; i++)
+    {
+        sleep(1);
+    }
+}
